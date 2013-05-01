b058278ed0b630e923a0027f84bb613d5f8717b2
Akiyoshi Tanaka <akkunchoi@gmail.com>
2013-05-01 18:59:46 +0900

    refactor

--- lib/diary_log.rb
+++ lib/diary_log.rb
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'date'
-
 require 'diary_log/record'
 require 'diary_log/parser'
 require 'diary_log/event'
@@ -9,6 +7,7 @@ require 'diary_log/pattern'
 require 'diary_log/gcal'
 require 'diary_log/controller'
 
+require 'date'
 require 'yaml'
 require 'pp'
 
@@ -29,48 +28,54 @@ class Hash
   end
 end
 
-config = YAML.load_file("config.yml").symbolize_keys_recursive
-config[:root] = File.dirname(File.dirname(__FILE__))
+module DiaryLog
+  class Main
+    def initialize
+      config = YAML.load_file("config.yml").symbolize_keys_recursive
+      config[:root] = File.dirname(File.dirname(__FILE__))
 
-# Override with argv
-require 'optparse'
-require 'active_support'
-opt = OptionParser.new
+      # Override with argv
+      require 'optparse'
+      require 'active_support'
+      opt = OptionParser.new
 
-opt.on('--gcal-insert') {|v| config[:gcal][:insert] = true }
-opt.on('--gcal-insert-dry-run') {|v| config[:gcal][:insert_dry_run] = true}
-opt.on('--day-ago NUM') {|v| config[:period][:day_ago] = v }
-opt.on('--show-rests') {|v| config[:show_rests] = true}
-opt.on('--show-records') {|v| config[:show_records] = true}
-opt.on('--show-events') {|v| config[:show_events] = true}
-opt.on('--last-week [WEEK]') do |v| 
-  v = 1 if v.nil? || v == ""
-  v = Integer(v)
+      opt.on('--gcal-insert') {|v| config[:gcal][:insert] = true }
+      opt.on('--gcal-insert-dry-run') {|v| config[:gcal][:insert_dry_run] = true}
+      opt.on('--day-ago NUM') {|v| config[:period][:day_ago] = v }
+      opt.on('--show-rests') {|v| config[:show_rests] = true}
+      opt.on('--show-records') {|v| config[:show_records] = true}
+      opt.on('--show-events') {|v| config[:show_events] = true}
+      opt.on('--last-week [WEEK]') do |v| 
+        v = 1 if v.nil? || v == ""
+        v = Integer(v)
 
-  base = Date.today + 1
-  v.times do
-    base = base.prev_week
+        base = Date.today + 1
+        v.times do
+          base = base.prev_week
+        end
+        s = base - 1 # sunday start
+        e = base + 6  # this sunday
+        config[:period][:day_ago] = nil
+        config[:period][:day_since] = s.strftime("%Y-%m-%d")
+        config[:period][:day_until] = e.strftime("%Y-%m-%d")
+      end
+      opt.on('--since SINCE') do |v| 
+        # TODO validation
+        config[:period][:day_ago] = nil
+        config[:period][:day_since] = v
+      end
+      opt.on('--until UNTIL') do |v| 
+        # TODO validation
+        config[:period][:day_ago] = nil
+        config[:period][:day_until] = v
+      end
+
+      opt.parse!(ARGV)
+
+      ctrl = DiaryLog::Controller.new(config)
+      ctrl.run
+    end
   end
-  s = base - 1 # sunday start
-  e = base + 6  # this sunday
-  config[:period][:day_ago] = nil
-  config[:period][:day_since] = s.strftime("%Y-%m-%d")
-  config[:period][:day_until] = e.strftime("%Y-%m-%d")
 end
-opt.on('--since SINCE') do |v| 
-  # TODO validation
-  config[:period][:day_ago] = nil
-  config[:period][:day_since] = v
-end
-opt.on('--until UNTIL') do |v| 
-  # TODO validation
-  config[:period][:day_ago] = nil
-  config[:period][:day_until] = v
-end
-
-opt.parse!(ARGV)
-
-ctrl = DiaryLog::Controller.new(config)
-ctrl.run
 
 
